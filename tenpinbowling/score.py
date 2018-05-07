b4bd3164257200db9c03e65d46c9b1aa8dcc562a
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-05-07 21:55:21 +0100

    Solution passes the current not completed tests.

--- tenpinbowling/score.py
+++ tenpinbowling/score.py
@@ -1,20 +1,35 @@
 import numpy
 
-def getScore(rolls):
+def checkStrike(frame):
+    if frame[0] == 10:
+        return True
+    else:
+        return False
+
+def checkSpare(frame):
+    if frame[0] + frame[1] == 10 and frame[0] < 10:
+        return True
+    else:
+        return False
+
+def getScore(frames):
     
-    #print(rolls)
-    score = rolls[0]
-    # print(score)
-    score += rolls[1]
-    # print(score)
-    for i in range(2, len(rolls)):
-        
-        score += rolls[i]
+    print(frames)
+    score = 0
+    
+    for i in range(0, 3):
+        score += sum(frames[i])
+        if checkStrike(frames[i]):
+            score += sum(frames[i+1])
+            if checkStrike(frames[i+1]):
+                score += frames[i+2][0]
+
+        print(frames[i])
 
         # Calculate strike
-        if rolls[i-2] == 10:
-            score += rolls[i]
-            score += rolls[i-1]
+        # if rolls[i-2] == 10:
+        #     score += rolls[i]
+        #     score += rolls[i-1]
 
         # Calculate spare
         # if rolls[i-2] + rolls[i-1] == 10:
