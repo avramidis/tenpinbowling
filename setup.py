d0e6ff48abaabec45c4f03269322c32711fa793a
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-05-08 21:15:14 +0100

    Added a setup script for the installation of the package.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='tenpinbowling',
+      version='1.0',
+      description='Python package that calculates the score of a 10 pin bowling game.',
+      packages=['tenpinbowling'],
+      license='MIT'
+     )
